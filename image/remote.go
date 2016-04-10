a72e887c943ad6ba3c8461289da4f41d87ee2824
odacremolbap <odacremolbap@gmail.com>
2016-04-10 15:17:23 +0200

    download image from URL

--- image/remote.go
+++ image/remote.go
@@ -1,7 +1,59 @@
 package image
 
-// downloadImage retrieves a image from internet
-func downloadImage(image string, root string) (string, error) {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
 
-	return "", nil
+	log "github.com/sirupsen/logrus"
+)
+
+// downloadImage retrieves a image from internet
+// TODO support 302 redirections
+func downloadImage(image string, root string) (string, error) {
+	log.Debugf("downloading  image %s", image)
+
+	imageURL, err := url.Parse(image)
+	if err != nil {
+		return "", err
+	}
+
+	// get name of the file to download
+	// if name is empty use a default one
+	fileName := imageURL.Path[(strings.LastIndex(imageURL.Path, "/") + 1):]
+	if fileName == "" {
+		fileName = "isolate-image"
+	}
+
+	fileName = filepath.Join(root, fileName)
+
+	// TODO create channel to get progress
+	resp, err := http.Get(image)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("Remote site returned status %s.", resp.Status)
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	read, err := io.Copy(file, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	log.Printf("read %d bytes to %s", read, fileName)
+
+	return fileName, nil
 }
