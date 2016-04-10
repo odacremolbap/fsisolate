07dc10c559b98eaea8b613055fe48a8827b0132d
odacremolbap <odacremolbap@gmail.com>
2016-04-10 14:46:33 +0200

    extract image Although it's a normal extract, not image related. Might be moved to a fileutils package in the future

--- image/extract.go
+++ image/extract.go
@@ -1,7 +1,64 @@
 package image
 
+import (
+	"archive/tar"
+	"io"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+)
+
 // extractImage retrieves a image from internet
 // TODO not all tar formats supported
+// TODO if extraction fails halfway, defer to delete remaining files
 func extractImage(image string, root string) error {
+	log.Debugf("decompressing %s \nto %s", image, root)
+
+	imgRead, err := os.Open(image)
+	if err != nil {
+		return err
+	}
+	defer imgRead.Close()
+
+	tr := tar.NewReader(imgRead)
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			// exit loop at end of stream
+			break
+		}
+
+		// extract error
+		if err != nil {
+			return err
+		}
+
+		path := filepath.Join(root, header.Name)
+		fi := header.FileInfo()
+
+		// restore dir
+		if fi.IsDir() {
+			if err = os.MkdirAll(path, fi.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// restore file
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, fi.Mode())
+		if err != nil {
+			return err
+		}
+
+		// write file contents
+		defer file.Close()
+		_, err = io.Copy(file, tr)
+		if err != nil {
+			return err
+		}
+
+	}
 	return nil
 }
